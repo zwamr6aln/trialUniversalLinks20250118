3b1daa30b41131fab04fc105cdc97621f7455c35
a <a@a>
2025-01-18 11:08:46 +0900

    Initial Commit

---
+++ trialUniversalLinks20250118/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trialUniversalLinks20250118
+//
+//  Created by FullName241111 on 2025/01/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trialUniversalLinks20250118/trialUniversalLinks20250118App.swift
@@ -0,0 +1,17 @@
+//
+//  trialUniversalLinks20250118App.swift
+//  trialUniversalLinks20250118
+//
+//  Created by FullName241111 on 2025/01/18.
+//
+
+import SwiftUI
+
+@main
+struct trialUniversalLinks20250118App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
