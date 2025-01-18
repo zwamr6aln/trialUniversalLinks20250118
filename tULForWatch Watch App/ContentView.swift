2c7c2191f3835452b116161c68ccaacfb8521b65
a <a@a>
2025-01-18 11:52:35 +0900

    a

---
+++ tULForWatch Watch App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  tULForWatch Watch App
+//
+//  Created by FullName241111 on 2025/01/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ tULForWatch Watch App/tULForWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  tULForWatchApp.swift
+//  tULForWatch Watch App
+//
+//  Created by FullName241111 on 2025/01/18.
+//
+
+import SwiftUI
+
+@main
+struct tULForWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
